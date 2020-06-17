bbe59ed17f0782e85191bd047201f50e869780c8
PadoSensei <44815835+PadoSensei@users.noreply.github.com>
2020-06-17 18:26:22 -0300

    Fixed Timer / Progress Bar

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,5 +1,21 @@
 <script>
   import ProgressBar from "./ProgressBar.svelte";
+  const totalSeconds = 20;
+  let secondsLeft = totalSeconds;
+  let isRunning = false;
+  $: progress = ((totalSeconds - secondsLeft) / totalSeconds) * 100;
+
+  function startTimer() {
+    isRunning = true;
+    const timer = setInterval(() => {
+      secondsLeft -= 1;
+      if (secondsLeft === 0) {
+        clearInterval(timer);
+        isRunning = false;
+        secondsLeft = 0;
+      }
+    }, 1000);
+  }
 </script>
 
 <style>
@@ -12,13 +28,25 @@
     width: 100%;
     margin: 10px 0;
   }
+
+  .start[disabled] {
+    background-color: rgb(194, 194, 194);
+    cursor: not-allowed;
+  }
 </style>
 
 <div bp="grid">
-  <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:</h2>
+  <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:{secondsLeft}</h2>
 
 </div>
 
-<ProgressBar />
-
-<button class="start">Start</button>
+<ProgressBar {progress} />
+<div bp="grid">
+  <button
+    disabled={isRunning}
+    on:click={startTimer}
+    bp="offset-5@md 4@md 12@sm"
+    class="start">
+    Start
+  </button>
+</div>
